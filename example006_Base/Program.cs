45ccc6925c46b2b13bdfc3064de92e65d715f1a9
AntonAntonkin <antonkin_anton@mail.ru>
2022-12-14 00:16:55 +0500

    проверка без скобок

---
+++ example006_Base/Program.cs
@@ -0,0 +1,34 @@
+﻿int a = new Random().Next(0, 100);
+int b = new Random().Next(0, 100);
+int c = new Random().Next(0, 100);
+int d = new Random().Next(0, 100);
+int e = new Random().Next(0, 100);
+int max = a;
+string let = "a";
+if (b > max)
+{
+    max = b;
+    let = "b";
+}
+if (c > max)
+{
+    max = c;
+    let = "c";
+}
+if (d > max)
+{
+    max = d;
+    let = "d";
+}
+if (e > max)
+{
+    max = e;
+    let = "e";
+}
+Console.WriteLine("a=" + a);
+Console.WriteLine("b=" + b);
+Console.WriteLine("c=" + c);
+Console.WriteLine("d=" + d);
+Console.WriteLine("e=" + e);
+Console.Write("Max is " + max + " (" + let + ")");
+
